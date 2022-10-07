5e8d831d859fa9195a83444133cc5999a87213f0
Olivier <oaudetyang@gmail.com>
2022-10-06 23:16:41 -0400

    uh

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
               />
             </div>
           </div>
-          <div className="mt-32 xl:max-w-[30rem] xl:ml-[20%] mx-auto">
+          <div className="mt-20 xl:max-w-[30rem] mx-auto">
             <div className="mx-5">
               <h2 className="font-black text-5xl mb-7 text-shadow xl:text-right xl:mr-15">
                 Foundation myth
@@ -73,10 +73,10 @@ function App() {
                   )}
                 </p>
                 <p className="mb-5">
-                  Except, when Zeus was about to be born, Rhea asked Uranus
-                  and Ge for advice to save Zeus. Uranus and Ge sent Rhea to
-                  Lyctos in Crete, and asked her to bring her child there. Rhea
-                  hid Zeus in a cave in Mount Aegaeon{" "}
+                  Except, when Zeus was about to be born, Rhea asked Uranus and
+                  Ge for advice to save Zeus. Uranus and Ge sent Rhea to Lyctos
+                  in Crete, and asked her to bring her child there. Rhea hid
+                  Zeus in a cave in Mount Aegaeon{" "}
                   <a
                     href="/sources#6"
                     className="text-blue-500 visited:text-purple-700 underline"
@@ -105,6 +105,35 @@ function App() {
                       baby just saying
                     </p>
                   )}
+                </p>
+              </article>
+            </div>
+          </div>
+          <div className="mt-32 xl:max-w-[30rem] xl:ml-[20%] mx-auto">
+            <div className="mx-5">
+              <h2 className="font-black text-5xl mb-7 text-shadow xl:text-right xl:mr-5">
+                Appearance
+              </h2>
+              <article className="font-mono text-sm">
+                <p className="mb-5">
+                  Zeus is usually depicted as a man with a dark beard, his royal
+                  sceptre, his eagle, and his iconic lightning bolt, which he
+                  got from Kyklopes{" "}
+                  <a
+                    href="/sources#3"
+                    className="text-blue-500 visited:text-purple-700 underline"
+                  >
+                    [3]
+                  </a>
+                  . He was usually depicted with a long robe, but was sometimes
+                  depicted as nude. He often wears his crown of olive leaves{" "}
+                  <a
+                    href="/sources#4"
+                    className="text-blue-500 visited:text-purple-700 underline"
+                  >
+                    [4]
+                  </a>
+                  .
                 </p>
               </article>
             </div>
@@ -148,10 +177,10 @@ function App() {
                 .
               </p>
               <p className="mb-5">
-                Zeus was also quick to anger. For example, when Prometheus
-                stole fire from the Gods and gave it to humans, Zeus got
-                angry and tied him with chains, impaled him with a stick, and
-                sent an eagle to eat his liver everyday{" "}
+                Zeus was also quick to anger. For example, when Prometheus stole
+                fire from the Gods and gave it to humans, Zeus got angry and
+                tied him with chains, impaled him with a stick, and sent an
+                eagle to eat his liver everyday{" "}
                 <a
                   href="/sources#9"
                   className="text-blue-500 visited:text-purple-700 underline"
@@ -163,6 +192,32 @@ function App() {
               <p>Zeus was also described as “wise” in some ancient texts.</p>
             </article>
           </div>
+          <div className="mt-12 flex justify-center xl:block flex-col xl:ml-[20%]">
+            <h2 className="font-black text-5xl text-shadow mb-5">
+              What does he have command over?
+            </h2>
+
+            <article className="font-mono text-sm xl:max-w-[30rem] xl:ml-10">
+              <p className="mb-5">
+                Zeus was the King of the Gods and the god of sky, weather, law
+                and order, destiny and fate, kingship, and moral conduct{" "}
+                <a
+                  href="/sources#5"
+                  className="text-blue-500 visited:text-purple-700 underline"
+                >
+                  [5]
+                </a>
+                .
+                {golden && (
+                  <p className="text-yellow-400">
+                    Note: I kinda have a hard time believing Zeus was god of
+                    moral conduct
+                  </p>
+                )}
+              </p>
+            </article>
+          </div>
+
           <div className="mt-12">
             <h2 className="mb-7 xl:mr-[20%] xl:text-right">
               <div className="font-black text-shadow text-5xl">
@@ -186,11 +241,11 @@ function App() {
             </h2>
             <article className="font-mono text-sm xl:max-w-[30rem] xl:ml-[30%]">
               <p className="mb-5">
-                The temple of Zeus was a temple in Athens. It was built
-                around 460 BC by the architect Libon of Elis, and was made of
-                shell conglomerate. The exposed surfaces were covered with a
-                coat of white stucco. Its porch and rear porch provided access
-                to the central hall, which was divided into ailes by two rows of
+                The temple of Zeus was a temple in Athens. It was built around
+                460 BC by the architect Libon of Elis, and was made of shell
+                conglomerate. The exposed surfaces were covered with a coat of
+                white stucco. Its porch and rear porch provided access to the
+                central hall, which was divided into ailes by two rows of
                 columns.
               </p>
 
@@ -203,13 +258,12 @@ function App() {
 
               <p className="mb-5">
                 Inside the temple was one of the seven wonders of the ancient
-                world, the Statue of Zeus. It was made by the Athenian
-                sculptor Phidias, who represented the god seated on a throne.
+                world, the Statue of Zeus. It was made by the Athenian sculptor
+                Phidias, who represented the god seated on a throne.
               </p>
               <p className="mb-5">
-                Beside the temple was the great altar of Olympian Zeus, in
-                which was a mound of the ashes of the animals sacrificed to
-                Zeus.
+                Beside the temple was the great altar of Olympian Zeus, in which
+                was a mound of the ashes of the animals sacrificed to Zeus.
               </p>
             </article>
           </div>
